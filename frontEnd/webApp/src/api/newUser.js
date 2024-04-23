1b99fb1efcc00c74d47ef501b2a1d42ebbdcf994
NirashaSewwandi <e18329@eng.pdn.ac.lk>
2024-04-23 17:40:49 +0530

    Add correction for endpoint

--- frontEnd/webApp/src/api/newUser.js
+++ frontEnd/webApp/src/api/newUser.js
@@ -14,6 +14,6 @@ export const newUser = {
   // url: "http://new-user-api-608428337:9091/newUser",
   
   // url: "https://02c6919b-de66-4977-ad24-2353ebcd8048-dev.e1-us-east-azure.choreoapis.dev/mhmp/new-user-api/newuser-2ab/v1.0/newUser",
-  url: "https://02c6919b-de66-4977-ad24-2353ebcd8048-dev.e1-us-east-azure.choreoapis.dev/mhmp/add-new-user/newuser-2ab/v1.0",
+  url: "https://02c6919b-de66-4977-ad24-2353ebcd8048-dev.e1-us-east-azure.choreoapis.dev/mhmp/add-new-user/newuser-2ab/v1.0/newUser",
   data: {},
 };
